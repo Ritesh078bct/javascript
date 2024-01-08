899ea4d253e63db02dc35acc04ac0ee2996f747a
Ritesh078bct <078bct068@student.ioepc.edu.np>
2024-01-08 22:16:16 +0545

    quiz app completed using javascript

--- QUIZ/script.js
+++ QUIZ/script.js
@@ -44,27 +44,76 @@ function startQuiz()
     showQuestion();
 }
 function showQuestion(){
-//   resetState();
+  resetState();
 
     question.innerHTML=currentQuestionIndex+1+"."+questions[currentQuestionIndex].question;
     // console.log(questions[index].question)
     // console.log(q.question);
     let answers=questions[currentQuestionIndex].
-    answers.forEach(element=>{
+    answers.forEach(answer=>{
 
         let createButton=document.createElement("button");
-        createButton.innerText=element.text;
+        createButton.innerText=answer.text;
         answerButton.appendChild(createButton);
-        console.log(element.text);
+        // console.log(answer.text);
+        if(answer.correct){
+            createButton.dataset.correct=answer.correct;
+        }
+        createButton.addEventListener("click",selectAnswer)
 
       });
     
 }
+function selectAnswer(e){
+    selectButton=e.target;
+    console.log(selectButton);
+    if(selectButton.dataset.correct==="true"){
+        selectButton.style.backgroundColor="rgba(112, 239, 77, 0.655)";
+        score++;   
+    }
+    else{
+        selectButton.style.backgroundColor="rgba(203, 38, 38, 0.744)";
+    }
+    Array.from(answerButton.children).forEach(button=>{
+        if(button.dataset.correct==="true"){
+            button.style.backgroundColor="rgba(112, 239, 77, 0.655)";
+        }
+        button.disabled=true;
+    });
+   nextButton.style.display="block";
 
+}
 function resetState(){
     nextButton.style.display="none";
-    // while(answerButton.firstChild){
-    //     answerButton.remove(answerButton.firstChild);
-    // }
+  
+    while(answerButton.firstChild){
+        answerButton.removeChild(answerButton.firstChild);
+    }
 }
+function showScore(){
+    resetState();
+    document.querySelector(".quiz h2").innerText=`Your score is: ${score}`;
+    nextButton.innerText="play Again";
+    nextButton.style.display="block";
+   
+
+}
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex<questions.length){
+        showQuestion();
+    }
+    else{
+        showScore();
+    }
+}
+nextButton.addEventListener("click",()=>{
+    if(currentQuestionIndex<questions.length){
+        handleNextButton();
+    }
+    else{
+        startQuiz();//play again
+    }
+});
+
 startQuiz();
